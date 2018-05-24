954d6745fa98d73e21bb898585c629866592e79e
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-05-24 14:44:17 -0400

    added gradient function

--- Uniformity_Evaluation.py
+++ Uniformity_Evaluation.py
@@ -56,13 +56,52 @@ def partial_derivative(f, i):
 
         # now that we have the function treating all other variables as constants except for the ith one, we have a
         # normal 1 dimensional derivative; evaluate it at the ith coordinate of the point
-        # set dx to be a sufficiently small value
-        normal_derivative = derivative(fixed_value_except_ith, point[i], dx=1e-6)
+        normal_derivative = derivative(fixed_value_except_ith, point[i])
         return normal_derivative
 
     # return the function that can evaluate partial derivative at a point, without giving it any input.
     # so this is the partial derivative function
     return evaluate_partial_derivative
+
+
+def gradient(f, n):
+    """
+    NAME:
+        gradient
+
+    PURPOSE:
+        Given an n-dimensional real-valued function, f, return the gradient function of f.
+
+    INPUT:
+        f = a differentiable function that takes n number to 1 number
+        n = number of variables that f takes
+
+    OUTPUT:
+        combined_function = a vector-valued function, with n component functions, each of which takes n numbers to 1
+                            number. The component functions are the partial derivative of f with respect to one of its
+                            input variable, in the right order
+
+    HISTORY:
+        2018-05-23 - Written - Samuel Wong
+    """
+    # initialize a list of component functions with n components
+    component_functions = [None for i in range(n)]
+
+    # loop through each dimension of input of f
+    for i in range(n):
+        # the ith component function of the gradient is the ith partial derivative of f
+        component_functions[i] = partial_derivative(f, i)
+
+    # create the combined gradient fucntion
+    def combined_function(x):
+        result = np.empty(n)
+        for i in range(n):
+            result[i] = component_functions[i](x)
+        return result
+
+    # return the function we just created. Note that this is just the function, without the argument
+    # this means that the result of the return can be called with input
+    return combined_function
 
 
 def evaluate_uniformity(f, x, W):
