934530c83121ec84a7302137ad6ae400dcc105b5
Mathew Bub <mathew.bub@gmail.com>
2018-05-23 22:49:27 -0400

    Added phase space search functionality.

--- search_phase_space.py
+++ search_phase_space.py
@@ -6,35 +6,34 @@ Last Revision Date: 2018-05-22
 This module contains the serach6d function, which queries the Gaia archive for
 stars close to a given point in phase space, using a galacitc coordiante frame.
 """
-
+import numpy as np
+import astropy.units as u
+from astropy.coordinates import SkyCoord
 from astroquery.gaia import Gaia
 from galpy.util.bovy_coords import lb_to_radec
-import numpy as np
-from astropy.coordinates import SkyCoord
-import astropy.units as u
 
 ra_ngp, dec_ngp = lb_to_radec(0, np.pi/2, epoch=None)
 k = (u.kpc*u.mas/u.yr).to(u.km*u.rad/u.s)
 
-def search_phase_space():
+def search_phase_space(x, y, z, vx, vy, vz, epsilon, A):
     import warnings
     warnings.filterwarnings("ignore")
     
     query = """
     SELECT *
     FROM (SELECT *,
-          d * cosb * cosl as x,
-          d * cosb * sinl as y,
-          d * sinb as z,
-          radial_velocity * cosb * cosl - {2} * d * pmb * sinb * cosl - {2} * d * pml_cosb * sinl as vx,
-          radial_velocity * cosb * sinl - {2} * d * pmb * sinb * sinl + {2} * d * pml_cosb * cosl as vy,
-          radial_velocity * sinb + {2} * d * pmb * cosb as vz
+          d*cosb*cosl as x,
+          d*cosb*sinl as y,
+          d*sinb as z,
+          vr*cosb*cosl - {0}*d*(pmb*sinb*cosl + pml_cosb*sinl) as vx,
+          vr*cosb*sinl - {0}*d*(pmb*sinb*sinl - pml_cosb*cosl) as vy,
+          vr*sinb + {0}*d*pmb*cosb as vz
     FROM (SELECT *,
-          pmra * cosphi + pmdec * sinphi as pml_cosb,
-          pmdec * cosphi - pmra * sinphi as pmb
+          pmra*cosphi + pmdec*sinphi as pml_cosb,
+          pmdec*cosphi - pmra*sinphi as pmb
     FROM (SELECT *,
-          SIN(RADIANS(ra) - {0}) * COS({1}) / cosb as sinphi,
-          (SIN({1}) - sindec * sinb) / (cosdec * cosb) as cosphi
+          COS({1})*SIN(RADIANS(ra)-{2}) / cosb as sinphi,
+          (SIN({1}) - sindec*sinb) / (cosdec*cosb) as cosphi
     FROM (SELECT TOP 5 *,
           SIN(RADIANS(dec)) as sindec,
           COS(RADIANS(dec)) as cosdec,
@@ -42,16 +41,14 @@ def search_phase_space():
           COS(RADIANS(b)) as cosb,
           SIN(RADIANS(l)) as sinl,
           COS(RADIANS(l)) as cosl,
-          1/parallax as d
+          1/parallax as d,
+          radial_velocity as vr
     FROM gaiadr2.gaia_source
-        WHERE 1=CONTAINS(
-        POINT('ICRS', ra, dec),
-        CIRCLE('ICRS',
-        COORD1(EPOCH_PROP_POS(176.42881667,-64.84151667,215.7800,2662.0360,
-                              -345.1830,0,2000,2015.5)),
-        COORD2(EPOCH_PROP_POS(176.42881667,-64.84151667,215.7800,2662.0360,
-                              -345.1830,0,2000,2015.5)), 1.0))
-        AND radial_velocity IS NOT NULL) tab0) tab1) tab2) tab3
-    """.format(ra_ngp, dec_ngp, k)
+        WHERE radial_velocity IS NOT NULL) tab0) tab1) tab2) tab3
+    WHERE SQRT(POWER(x-{3},2) + POWER(y-{4},2) + POWER(z-{5},2) +
+    {9}*(POWER(vx-{6},2) + POWER(vy-{7},2) + POWER(vz-{8},2))) < {10}
+    """.format(k, dec_ngp, ra_ngp, x, y, z, vx, vy, vz, A, epsilon)
     job = Gaia.launch_job_async(query)
     return job.get_results()
+
+# (x-{3})**2 + (y-{4})**2 + (z-{5})**2 + {9}*((vx-{6})**2 + (vy-{7})**2 + (vz-{8})**2) 
