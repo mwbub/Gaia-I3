bbe16f568734bfb1998999c006d8740902b8590b
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-05-25 11:39:34 -0400

    finished orthogonal complement function

--- Linear_Algebra.py
+++ Linear_Algebra.py
@@ -10,41 +10,55 @@ FUNCTIONS:
     orthogonal_complement: this function returns the orthogonal complement space of a given set of vectors
 
 HISTORY:
-    2018-05-24 - Written - Samuel Wong
+    2018-05-25 - Written - Samuel Wong
 """
 import numpy as np
-from sympy import Matrix
+from sympy import Matrix, GramSchmidt
 
 
-def orthogonal_complement(n, V):
+def orthogonal_complement(V):
     """
     NAME:
         orthogonal_complement
 
     PURPOSE:
-        Given the general n dimensional real number space, R^n, and V, a set of m vectors (m < n) in the space,
-        find the orthogonal complement to the space span by the m vectors. Then return an orthonormal basis of the
-        dimensionally reduced space.
+        Given V, a set of m number of n-dimensional vectors, find the orthogonal complement to the space span by
+        the m vectors. Then return an orthonormal basis of the orthogonal space.
 
     INPUT:
-        n = the number of dimensions of the space. The vector space is R^n.
-        V = a numpy array containing m number of linearly independent n-dimensional vectors, where m < n
+        V = a numpy array containing m number of n-dimensional vectors; the vectors are represented in rows
 
     OUTPUT:
-        W = a numpy array containing (m-n) number of linearly independent n-dimensional vectors that form an
+        W = a numpy array containing a number of linearly independent n-dimensional vectors that form an
             orthonormal basis of the orthogonal complement of span(V)
 
+    WARNING: the shape of V must be in the form of (m,n), with vectors written in row. Also, the function will return
+             an empty array if V already has maximum rank
+
     HISTORY:
-        2018-05-24 - Written - Samuel Wong
+        2018-05-25 - Written - Samuel Wong
     """
-    # get the number of vectors in V
-    m = np.size(V)
-    # take the transpose of V so that the vectors spanning the space are written in column
-    A = V.T  # A is now an m by n matrix
+    # the actual code is simple but the math behind is a bit tricky. I will explain the behind the scene math here.
+    # take the transpose of V so that the vectors spanning the space are written in column: A = V.T
+    # A is an n by m matrix
     # R(A) is the column space of A, or the range of A, which is also the space to be eliminated from R^n to get the
     # orthogonal complement
-    # so we want W = R(A)^{perp}. But by a basic theorem, R(A)^{perp} = N(A.T) = N(V)
-    # so we just need null space of V
-    W = null_space(V)
-    W = orthonormal(W)
+    # so we want W = R(A)^{perp}, the orthogonal complement of range of A
+    # But by a basic theorem, R(A)^{perp} = N(A.T) = N(V)
+    # so we just need to find the null space of V
+
+    # convert V to a sympy Matrix object
+    V = Matrix(V)
+    # get the null space of V. The vectors of the null space are stored as rows in W.
+    W = V.nullspace()
+    # apply the GramScmidt process to orthogonalize the vectors in W. Adding the True statement also normalize them
+    W = GramSchmidt(W, True)
+    # convert W back into numpy array
+    W = np.array(W)
     return W
+
+v1 = np.array([1,2,3,4,5,6])
+v2 = np.array([2,3,4,5,6,7])
+V = np.array([v1,v2])
+W = orthogonal_complement(V)
+print(W)
