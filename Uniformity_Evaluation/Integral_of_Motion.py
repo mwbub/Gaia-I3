5c1ba5b577a52a80d63971cb54d4894ef718fa4f
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-05-25 15:19:39 -0400

    added all docstring

--- Uniformity_Evaluation/Integral_of_Motion.py
+++ Uniformity_Evaluation/Integral_of_Motion.py
@@ -3,11 +3,13 @@ NAME:
     Integral_of_Motion
 
 PURPOSE:
-    This module contains energy and angular momentum functions.
+    This module contains energy and angular momentum functions. It also contains cartesian to cylindrical conversion.
 
 FUNCTIONS:
     Energy: Ths function takes the position and velocity of a star and returns its toal energy (per mass)
-    L_z: This functiont takes the position and velocity of a star and returns its angular momentum in the z-direction
+    L_z: This functiont takes the position and velocity of a star and returns its angular momentum (per mass)
+         in the z-direction
+    cartesian_to_cylindrical: Convert position and velocity
 
 HISTORY:
     2018-05-25 - Written - Samuel Wong
@@ -43,7 +45,7 @@ def cartesian_to_cylindrical(x, y, z, vx, vy, vz):
         vz = velocity in z
 
     HISTORY:
-        2018-05-24 - Written - Samuel Wong
+        2018-05-25 - Written - Samuel Wong
     """
     R = np.sqrt(x**2 + y**2)
     phi = np.arctan(y/x)
@@ -53,17 +55,59 @@ def cartesian_to_cylindrical(x, y, z, vx, vy, vz):
 
 
 def Energy(x, y, z, vx, vy, vz):
+    """
+    NAME:
+        Energy
+
+    PURPOSE:
+        Given 6 coordinates for the position and velocity of a star in Cartesian coordinate, return energy per mass
+
+    INPUT:
+        x =  x coordinate
+        y = y coordinate
+        z = z coordinate
+        vx = velocity in x
+        vy = velocity in y
+        vz = velocity in z
+
+    OUTPUT:
+        energy = total energy per mass
+
+    HISTORY:
+        2018-05-25 - Written - Samuel Wong
+    """
     R, phi, z, vR, vT, vz = cartesian_to_cylindrical(x, y, z, vx, vy, vz)
     potential = evaluatePotentials(MWPotential2014, R, z)
     kinetic = (vx**2 + vy**2 + vz**2)/2.
     energy = kinetic + potential
     return energy
 
-def L_z(x,y,z,vx,vy,vz):
+
+def L_z(x, y, z, vx, vy, vz):
+    """
+    NAME:
+        L_z
+
+    PURPOSE:
+        Given 6 coordinates for the position and velocity of a star in Cartesian coordinate, return angular
+         momentum around z-axis per mass
+
+    INPUT:
+        x =  x coordinate
+        y = y coordinate
+        z = z coordinate
+        vx = velocity in x
+        vy = velocity in y
+        vz = velocity in z
+
+    OUTPUT:
+        L_z = angular momentum around z-axis per mass
+
+    HISTORY:
+        2018-05-25 - Written - Samuel Wong
+    """
     # convert to cylindrical coordinate
     R, phi, z, vR, vT, vz = cartesian_to_cylindrical(x, y, z, vx, vy, vz)
     # evaluate the angular momentum
     result = R*vT
     return result
-
-
