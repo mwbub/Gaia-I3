7982e28cfabf262d36dd0fce0cd17c6802069766
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-05-24 22:25:43 -0400

    formatting changes in docstring

--- Uniformity_Evaluation.py
+++ Uniformity_Evaluation.py
@@ -1,28 +1,28 @@
 """
-    NAME:
-        Uniformity_Evaluation
+NAME:
+    Uniformity_Evaluation
 
-    PURPOSE:
-        This module contains a function that evaluates whether a given function is uniform along a given subspace.
-        In addition, it contains functions that return gradient and partial derivative of a given function.
+PURPOSE:
+    This module contains a function that evaluates whether a given function is uniform along a given subspace.
+    In addition, it contains functions that return gradient and partial derivative of a given function.
 
-    FUNCTIONS:
-        evaluate_uniformity: This function takes a differeniable function, a point, and the vectors generating
-                             a subspace, and return whether the function is uniform along the subspace at that point.
+FUNCTIONS:
+    evaluate_uniformity: This function takes a differeniable function, a point, and the vectors generating
+                         a subspace, and return whether the function is uniform along the subspace at that point.
 
-        gradient: This function takes a differentiable function and returns the gradient of that function
+    gradient: This function takes a differentiable function and returns the gradient of that function
 
-        partial_derivative: This function takes a function and the position of the input variable with respect to which
-                            the partial derivative is to be taken. Then it returns the partial derivative with respect
-                            to that variable.
+    partial_derivative: This function takes a function and the position of the input variable with respect to which
+                        the partial derivative is to be taken. Then it returns the partial derivative with respect
+                        to that variable.
 
-    WARNINGS:
-        This module assumes the given function takes a numpy array, which can contain an arbitrary number of values.
-        In particular, this code cannot handle a function that takes multiple input directly.
+WARNINGS:
+    This module assumes the given function takes a numpy array, which can contain an arbitrary number of values.
+    In particular, this code cannot handle a function that takes multiple input directly.
 
-    HISTORY:
-        2018-05-24 - Written - Samuel Wong
-    """
+HISTORY:
+    2018-05-24 - Written - Samuel Wong
+"""
 import numpy as np
 from scipy.misc import derivative
 
