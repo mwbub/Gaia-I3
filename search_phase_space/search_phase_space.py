e3a4cac7b33050f7708d0e46e35e524e08cb4a2a
Mathew Bub <mathew.bub@gmail.com>
2018-05-26 12:35:36 -0400

    Updated documentation and reorganized code layout

--- search_phase_space/search_phase_space.py
+++ search_phase_space/search_phase_space.py
@@ -1,18 +1,18 @@
 """ 
 Filename: search_phase_space.py
 Author: Mathew Bub
-Last Revision Date: 2018-05-24
+Last Revision Date: 2018-05-26
 
 This module contains the search_phase_space function, which queries the 
 Gaia archive for stars close to a given point in phase space, using a galactic 
 coordinate frame. Coordinate transformations are dervied from Bovy (2011).
 [https://github.com/jobovy/stellarkinematics/blob/master/stellarkinematics.pdf]
 """
-import astropy.units as u
-from astropy.coordinates import SkyCoord
-from astroquery.gaia import Gaia
-from galpy.util.bovy_coords import lb_to_radec
 import numpy as np
+import astropy.units as u
+from astroquery.gaia import Gaia
+from astropy.coordinates import SkyCoord
+from galpy.util.bovy_coords import lb_to_radec
 
 # ra and dec of the north galactic pole
 ra_ngp, dec_ngp = lb_to_radec(0, np.pi/2, epoch=None)
@@ -58,9 +58,6 @@ def search_phase_space(x, y, z, vx, vy, vz, epsilon, v_scale=1.0, cone_r=None):
     OUTPUT:
         astropy Table, containing stars from the Gaia DR2 RV catalogue that are
         within a distance of epsilon from the point (x, y, z, vx, vy, vz)
-        
-    HISTORY:
-        2018-05-24 - Written - Mathew Bub
     """
     import warnings
     warnings.filterwarnings("ignore")
@@ -73,24 +70,30 @@ def search_phase_space(x, y, z, vx, vy, vz, epsilon, v_scale=1.0, cone_r=None):
     vy = u.Quantity(vy, u.km/u.s)
     vz = u.Quantity(vz, u.km/u.s)
     
-    # get the ra and dec of the point (x, y, z), for use in a cone search
-    # to limit the size of the intial query
-    gal_coord = SkyCoord(frame='galactic', representation_type='cartesian', 
-                         u=x, v=y, w=z)
-    icrs_coord = gal_coord.transform_to('icrs')
-    cone_ra, cone_dec = icrs_coord.ra.value, icrs_coord.dec.value
-    
+    # distance check to limit the size of the initial query
     d = np.sqrt(x.value**2 + y.value**2 + z.value**2)
-    limiting_condition = ""
+    limiting_condition = "AND ABS({} - 1/parallax) < {}".format(d, epsilon)
+    
+    # add a cone search if the search sphere does not contain the Sun, or if 
+    # cone_r is set manually
     if d > epsilon or cone_r is not None:
+        
+        # get the ra and dec of the point (x, y, z) for use in the cone search
+        galactic_coord = SkyCoord(frame='galactic', u=x, v=y, w=z,
+                                  representation_type='cartesian')
+        icrs_coord = galactic_coord.transform_to('icrs')
+        cone_ra, cone_dec = icrs_coord.ra.value, icrs_coord.dec.value
+        
+        # calculate the minimal cone that will completely encompass the sphere
         if cone_r is None:
             h = d - epsilon**2 / d
             r = (epsilon / d)*np.sqrt(d**2 - epsilon**2)
             cone_r = np.degrees(np.arctan(r / h))
-        limiting_condition += ("AND 1=CONTAINS(POINT('ICRS', ra, dec), "
-                               "CIRCLE('ICRS', {}, {}, {}))\n\t"
+            
+        # cone search to further limit the initial query
+        limiting_condition += ("\n\tAND 1=CONTAINS(POINT('ICRS', ra, dec), "
+                               "CIRCLE('ICRS', {}, {}, {}))"
                                ).format(cone_ra, cone_dec, cone_r)
-    limiting_condition += "AND ABS({}-1/parallax) < {}".format(d, epsilon)
     
     # query parameters
     params = (k, dec_ngp, ra_ngp, limiting_condition, x.value, y.value,
@@ -111,7 +114,7 @@ def search_phase_space(x, y, z, vx, vy, vz, epsilon, v_scale=1.0, cone_r=None):
           pmra*cosphi + pmdec*sinphi AS pml,
           pmdec*cosphi - pmra*sinphi AS pmb
     FROM (SELECT *,
-          COS({1})*SIN(RADIANS(ra)-({2})) / cosb AS sinphi,
+          COS({1})*SIN(RADIANS(ra) - ({2})) / cosb AS sinphi,
           (SIN({1}) - sindec*sinb) / (cosdec*cosb) AS cosphi
     FROM (SELECT *,
           SIN(RADIANS(dec)) AS sindec,
@@ -123,7 +126,7 @@ def search_phase_space(x, y, z, vx, vy, vz, epsilon, v_scale=1.0, cone_r=None):
           1/parallax AS d
     FROM gaiadr2.gaia_source
         WHERE radial_velocity IS NOT NULL
-        {3}) tab0) tab1) tab2) tab3
+        {3}) table0) table1) table2) table3
     WHERE POWER({11},2) > POWER({4}-x,2) + POWER({5}-y,2) + POWER({6}-z,2) +
     (POWER({7}-vx,2) + POWER({8}-vy,2) + POWER({9}-vz,2))*POWER({10},2)
     """.format(*params)
