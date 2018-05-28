4868c65ddedee13e5a62fbf07a63d653c99fd2b2
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-05-28 13:34:16 -0400

    tiny change

--- check_uniformity_of_density/Test_Integral_of_Motion.py
+++ check_uniformity_of_density/Test_Integral_of_Motion.py
@@ -1,18 +1,24 @@
-from Integral_of_Motion import *
+from galpy.potential import MWPotential2014
+from Integral_of_Motion import cartesian_to_cylindrical
+import numpy as np
 
-#global variables
+# global variables
 # for the position, I took an example from online, the answer is (2, pi/6, 4)
 x = np.sqrt(3)
 y = 1.
 z = 4.
-# for the velocity, I chose the test to see whether the total velocity is the same, which is clealry 3 here
-vx = np.cbrt(3)
-vy = np.cbrt(3)
-vz = np.cbrt(3)
+# for the velocity, I chose the test to see whether the total velocity is the same, which is clealry 1 here
+vx = np.sqrt(1 / 3)
+vy = np.sqrt(1 / 3)
+vz = np.sqrt(1 / 3)
 
 
 def test_cartesian_to_cylindrical(x, y, z, vx, vy, vz):
-    return cartesian_to_cylindrical(x, y, z, vx, vy, vz)
+    R, phi, z, vR, vT, vz = cartesian_to_cylindrical(x, y, z, vx, vy, vz)
+    print('cylindrical position: ({},{},{})'.format(R, phi, z))
+    print('cylindrical velocity: ({},{},{})'.format(vR, vT, vz))
+    v = np.sqrt(vR**2 + vT**2 + vz**2)
+    print('net velocity = ', v)
 
-
+test_cartesian_to_cylindrical(x, y, z, vx, vy, vz)
 
