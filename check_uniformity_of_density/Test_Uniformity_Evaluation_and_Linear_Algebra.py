2d236832764080a536176948bd4328e1ec69db91
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-05-27 22:10:14 -0400

    added docstring of the firs test function

--- check_uniformity_of_density/Test_Uniformity_Evaluation_and_Linear_Algebra.py
+++ check_uniformity_of_density/Test_Uniformity_Evaluation_and_Linear_Algebra.py
@@ -8,8 +8,13 @@ def f(array):
     z = array[2]
     return 2*x*y + 3*y*z + z**2
 
-point = np.array([1., 1., 2.])
-gradient = np.array([2., 8., 7.])
-W = orthogonal_complement(np.array([gradient]))
-print(evaluate_uniformity(f, point, W))
+def test_evaluate_uniformity_and_orthogonal_complement():
+    # I hand calculate the gradient at the given point, ask orthogonal_complement function to calculate its orthogonal
+    # space. Then ask evaluate_uniformit to find the dot product against the othogonal space
+    # the answer should be 2 numbers very close to zero.
+    # success implies that both orthogonal_complement and gradient have to work properly
+    point = np.array([1., 1., 2.])
+    gradient = np.array([2., 8., 7.])
+    W = orthogonal_complement(np.array([gradient]))
+    print(evaluate_uniformity(f, point, W))
 
