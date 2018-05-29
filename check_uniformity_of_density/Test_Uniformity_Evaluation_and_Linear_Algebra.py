fixed the duplicate name gradient and changed to grad. Fixed test file accordingly

check_uniformity_of_density/Test_Uniformity_Evaluation_and_Linear_Algebra.py
@@ -2,6 +2,7 @@ from Linear_Algebra import *
 from Uniformity_Evaluation import *
 import numpy as np
 from sympy import Matrix
+from Integral_of_Motion import Energy, L_z
 
 def f(array):
     x = array[0]
@@ -13,6 +14,13 @@ def f(array):
 point = np.array([1., 1., 2.])
 gradient = np.array([2., 8., 7.])
 W = orthogonal_complement(np.array([gradient]))
+x = np.sqrt(3)
+y = 1.
+z = 4.
+vx = np.sqrt(1 / 3)
+vy = np.sqrt(1 / 3)
+vz = np.sqrt(1 / 3)
+a = np.array([x,y,z,vx,vy,vz])
 
 
 def test_evaluate_uniformity_and_orthogonal_complement(f, point, W):
@@ -20,6 +28,7 @@ def test_evaluate_uniformity_and_orthogonal_complement(f, point, W):
     # space. Then ask evaluate_uniformit to find the dot product against the othogonal space
     # the answer should be 2 numbers very close to zero.
     # success implies that both orthogonal_complement and gradient have to work properly
+    #del_f = gradient(f, np.size(point))
     print('dot product between gradient and its orthonormal space:', evaluate_uniformity(f, point, W))
 
 
@@ -34,7 +43,23 @@ def test_orthonormality(W):
         for j in range(m-1):
             if j != i:
                 print('w[{}] dot w[{}] = {}'.format(i, j, np.dot(W[i],W[j])))
+                
+def test_partial_of_energy():
+    #del_E = gradient(Energy, np.size(a))
+    print(type(grad))
+                
+def test_compatibility_with_integral_of_motion():
+    # get the gradient of energy and momentum at the point
+    del_E = grad(Energy, np.size(a))
+    del_Lz = grad(L_z, np.size(a))
+    del_E_a = del_E(a)
+    del_Lz_a = del_Lz(a)
+    print('energy vector = {}, momentum vector = {}'.format(del_E_a, del_Lz_a))
 
-test_orthonormality(W)
+
+#test_orthonormality(W)
 print()
 test_evaluate_uniformity_and_orthogonal_complement(f, point, W)
+print()
+test_compatibility_with_integral_of_motion()
+test_partial_of_energy()

check_uniformity_of_density/Uniformity_Evaluation.py
@@ -10,7 +10,7 @@ FUNCTIONS:
     evaluate_uniformity: This function takes a differeniable function, a point, and the vectors generating
                          a subspace, and return whether the function is uniform along the subspace at that point.
 
-    gradient: This function takes a differentiable function and returns the gradient of that function
+    grad: This function takes a differentiable function and returns the gradient of that function
 
     partial_derivative: This function takes a function and the position of the input variable with respect to which
                         the partial derivative is to be taken. Then it returns the partial derivative with respect
@@ -68,7 +68,7 @@ def partial_derivative(f, i):
     return evaluate_partial_derivative
 
 
-def gradient(f, n):
+def grad(f, n):
     """
     NAME:
         gradient
@@ -137,7 +137,7 @@ def evaluate_uniformity(f, x, W):
     # get the size of the domain spae
     n = np.size(x)
     # get the gradient of f
-    del_f = gradient(f, n)
+    del_f = grad(f, n)
     # evaluate the gradient at the given point
     del_f_x = del_f(x)
     # initialize directional_derivatives, where each component is gradient dotted with one of the vectors