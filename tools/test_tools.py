3902ad7fc2f3efb14eef6be688a318075f7053ee
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-05-31 12:26:50 -0400

    modified test tools

--- tools/test_tools.py
+++ tools/test_tools.py
@@ -2,19 +2,28 @@ import numpy as np
 from galpy.orbit import Orbit
 from galpy.potential import MWPotential2014
 from tools import *
+import os, sys
+outer_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(outer_path)
+from check_uniformity_of_density.Integral_of_Motion import *
 
-def test_natural_unit():
+def test_natural_unit(t):
     o = Orbit() # create sun's orbit
     o.turn_physical_off() # make galpy use natural coordinate
     ts = np.linspace(0,100,1000)
     o.integrate(ts,MWPotential2014)
-    natural_coord = np.array([o.x(),o.y(),o.z(),o.vx(),o.vy(),o.vz()])
+    natural_coord = np.array([o.x(t),o.y(t),o.z(t),o.vx(t),o.vy(t),o.vz(t)])
+    natural_energy = o.E(t)
     
     o.turn_physical_on() # make galpy use physical coordinate
-    physical_coord = np.array([o.x(),o.y(),o.z(),o.vx(),o.vy(),o.vz()])
+    physical_coord = np.array([o.x(t),o.y(t),o.z(t),o.vx(t),o.vy(t),o.vz(t)])
     my_coord = to_natural_units(np.array([physical_coord]))[0]
+    my_energy = Energy(my_coord)
+    
     
     print('galpy natural coord = ', natural_coord)
     print('my natural coord = ', my_coord)
+    print('galpy energy = ', natural_energy)
+    print('my energy = ', my_energy)
     
-test_natural_unit()
+test_natural_unit(38)
