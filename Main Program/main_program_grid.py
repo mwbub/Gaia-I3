3013c465fa5f9b01d05608a3e8b5dd2db081a42b
Mathew Bub <mathew.bub@gmail.com>
2018-06-06 13:43:47 -0400

    Tried to fix .gitignore

--- Main Program/main_program_grid.py
+++ Main Program/main_program_grid.py
@@ -19,7 +19,7 @@ check_uniformity_path =  os.path.abspath(os.path.join(outer_path, 'check_uniform
 sys.path.append(outer_path)
 sys.path.append(check_uniformity_path)
 # import relevant functions from different folders
-from search_phase_space.search_phase_space import *
+from search.search_online import *
 from check_uniformity_of_density.Integral_of_Motion import *
 from check_uniformity_of_density.Linear_Algebra import *
 from check_uniformity_of_density.Uniformity_Evaluation import *
