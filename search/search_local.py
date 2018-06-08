a6b409e1c8d1ba09ad77a394cb5a0251d8c674fa
Mathew Bub <mathew.bub@gmail.com>
2018-06-08 13:50:21 -0400

    Added tools/load.py. Reworked load behaviour in search_local to utilize tools/load.py. Reworked behaviour of search_local when reloading the module.

--- search/search_local.py
+++ search/search_local.py
@@ -8,64 +8,42 @@ Gaia archive for stars close to a given point in phase space, using a galactic
 coordinate frame. This version of the module uses a local downloaded copy of
 the Gaia DR2 RV catalogue.
 """
-import os
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
-import astropy.io.fits as pyfits
 from astropy import units
 from astropy.coordinates import SkyCoord
-from gaia_tools.load import path, download
+from tools import load
 
 try:
-    gaiarv
-    gaiarv_icrs
     gaiarv_gal
     gaiarv_galcen
+    _GAIA_LOADED
+    _PARALLAX_CUT
 except NameError:
     _GAIA_LOADED = False
-else:
-    _GAIA_LOADED = True
+    _PARALLAX_CUT = None
 
-def load_gaiarv(parallax_cut=True, load_errors=False):
-    global gaiarv
-    global gaiarv_icrs
+def load_gaiarv(parallax_cut=True):
     global gaiarv_gal
     global gaiarv_galcen
     global _GAIA_LOADED
-    
-    # download the Gaia DR2 RV catalogue if not already downloaded
-    file_paths= path.gaiarvPath()
-    if not np.all([os.path.exists(file_path) for file_path in file_paths]):
-        download.gaiarv()
+    global _PARALLAX_CUT
         
     # fields to load
     fields = ['ra', 'dec', 'parallax', 'pmra', 'pmdec', 'radial_velocity']
         
-    if parallax_cut:
-        # load parallax error percentage
-        fields.append('parallax_over_error')
-        
-    if load_errors:
-        # load the errors for each phase space coordinate
-        fields.extend(['ra_error', 'dec_error', 'parallax_error','pmra_error', 
-                       'pmdec_error', 'radial_velocity_error'])
-        
     # load the Gaia DR2 RV catalogue
-    gaiarv = np.lib.recfunctions.stack_arrays(
-        [np.array(pyfits.getdata(file_path, ext=1))[fields] for file_path 
-         in file_paths], autoconvert=True)
-    
-    if parallax_cut:
-        # cut out stars with parallax errors over 20%
-        gaiarv = gaiarv[gaiarv['parallax_over_error'] > 5]
+    data = load.gaiarv(fields=fields, parallax_cut=parallax_cut)
         
     # organize the catalogue into a SkyCoord object
-    gaiarv_icrs = SkyCoord(ra=gaiarv['ra']*units.deg, 
-                            dec=gaiarv['dec']*units.deg,
-                            distance=1/gaiarv['parallax']*units.kpc,
-                            pm_ra_cosdec=gaiarv['pmra']*units.mas/units.yr,
-                            pm_dec=gaiarv['pmdec']*units.mas/units.yr,
-                            radial_velocity=
-                            gaiarv['radial_velocity']*units.km/units.s)
+    gaiarv_icrs = SkyCoord(ra=data['ra']*units.deg,
+                           dec=data['dec']*units.deg,
+                           distance=1/data['parallax']*units.kpc,
+                           pm_ra_cosdec=data['pmra']*units.mas/units.yr,
+                           pm_dec=data['pmdec']*units.mas/units.yr,
+                           radial_velocity=
+                           data['radial_velocity']*units.km/units.s)
     
     # convert to galactic rectangular coordiantes
     gaiarv_gal = gaiarv_icrs.transform_to('galactic')
@@ -76,8 +54,10 @@ def load_gaiarv(parallax_cut=True, load_errors=False):
     gaiarv_galcen.representation_type = 'cartesian'
     
     _GAIA_LOADED = True
+    _PARALLAX_CUT = parallax_cut
 
-def search_phase_space(u0, v0, w0, U0, V0, W0, epsilon, v_scale):
+def search_phase_space(u0, v0, w0, U0, V0, W0, epsilon, v_scale,
+                       parallax_cut=True):
     """
     NAME:
         search_phase_space
@@ -115,9 +95,8 @@ def search_phase_space(u0, v0, w0, U0, V0, W0, epsilon, v_scale):
         consisting of stars within a distance of epsilon from the point
         (u0, v0, w0, U0, V0, W0)
     """
-    if not _GAIA_LOADED:
-        raise Exception("must call load_gaiarv before calling "
-                        "search_phase_space")
+    if not _GAIA_LOADED or parallax_cut != _PARALLAX_CUT:
+        load_gaiarv(parallax_cut=parallax_cut)
 
     # convert coordinates into consistent units
     u0 = units.Quantity(u0, units.kpc).value
@@ -155,7 +134,7 @@ def search_phase_space(u0, v0, w0, U0, V0, W0, epsilon, v_scale):
         return samples
     raise Exception("no results found")
     
-def get_entire_catalogue():
+def get_entire_catalogue(parallax_cut=True):
     """
     NAME:
         get_entire_catalogue
@@ -171,9 +150,8 @@ def get_entire_catalogue():
         Nx6 array of galactocentric coordinates of the form
         (x, y, z, vx, vy, vz) in [kpc, kpc, kpc, km/s, km/s, km/s]
     """
-    if not _GAIA_LOADED:
-        raise Exception("must call load_gaiarv before calling "
-                        "get_entire_cataloge")
+    if not _GAIA_LOADED or parallax_cut != _PARALLAX_CUT:
+        load_gaiarv(parallax_cut=parallax_cut)
     
     # organize the coordinates into an Nx6 array
     samples = np.stack([gaiarv_galcen.x.value,
