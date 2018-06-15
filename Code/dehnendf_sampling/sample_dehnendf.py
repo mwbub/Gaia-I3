8bf157e45a612f56386bbc321b20a96861e49cbc
Mathew Bub <mathew.bub@gmail.com>
2018-06-15 15:32:52 -0400

    Changed times to be expressed in natural units

--- Code/dehnendf_sampling/sample_dehnendf.py
+++ Code/dehnendf_sampling/sample_dehnendf.py
@@ -1,10 +1,11 @@
 import numpy as np
-import astropy.units as u
 from galpy.df import dehnendf
 from galpy.orbit import Orbit
 from galpy.potential import MWPotential2014
+from galpy.util.bovy_conversion import time_in_Gyr
 
-def get_3d_samples(n=1, rrange=None, integration_steps=100):
+def get_3d_samples(n=1, rrange=None, integration_time=1, 
+                   integration_steps=100):
     df = dehnendf()
     sampled_ROrbits = df.sample(n=n, rrange=rrange)
     
@@ -15,13 +16,15 @@ def get_3d_samples(n=1, rrange=None, integration_steps=100):
     kRz = vRz**2/2
     kR = kRz * np.random.random(n)
     kz = kRz - kR
+    
     vR = np.sqrt(2*kR) * np.sign(vRz)
     vz = np.sqrt(2*kz) * np.random.choice((1, -1), size=n)
     
     vxvv = [[R[i], vR[i], vT[i], 0, vz[i]] for i in range(n)]
     orbits = [Orbit(vxvv=vxvv[i]) for i in range(n)]
     
-    t = np.linspace(0, 500, integration_steps) * u.Myr
+    t = np.linspace(0, integration_time/time_in_Gyr(vo=220., ro=8.), 
+                    integration_steps)
     for o in orbits:
         o.integrate(t, MWPotential2014)
     
