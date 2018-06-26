1357a6653560fd0e326b6d821fc907f57062d8f0
Mathew Bub <mathew.bub@gmail.com>
2018-06-26 00:33:52 -0400

    Ran main on dehnendf with PowerSphericalPotential

--- Code/mock_data/dehnendf/run_main.py
+++ Code/mock_data/dehnendf/run_main.py
@@ -4,11 +4,12 @@ sys.path.append('../..')
 import numpy as np
 import astropy.units as u
 from astropy.coordinates import SkyCoord
-from main.main_program_cluster import main
+from main.main_program_cluster_psp import main
 from sample_near_gaia import load_mock_data
 
-data = load_mock_data(0, 0, 0, 0.5)
+data = load_mock_data(0, 0, 0, 0.5, use_psp=True)
 data = data[~np.any(np.isnan(data), axis=1)]
+'''
 galcen = SkyCoord(frame='galactocentric', representation_type='cartesian',
                   x=data.T[0], y=data.T[1], z=data.T[2], 
                   unit=[u.kpc, u.kpc, u.kpc])
@@ -16,5 +17,6 @@ gal = galcen.transform_to('galactic')
 gal.representation_type = 'cartesian'
 mask = (gal.u.value**2 + gal.v.value**2 + gal.w.value**2 < 0.5**2)
 data = data[mask]
+'''
 
 main(None, 'local', data)
