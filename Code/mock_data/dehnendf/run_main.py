deb4c2d926df11c9fc97c4d702c0eaa1c1de0f45
Mathew Bub <mathew.bub@gmail.com>
2018-06-28 21:14:17 -0400

    Re-configured run_main.py to run the new dehnendf data

--- Code/mock_data/dehnendf/run_main.py
+++ Code/mock_data/dehnendf/run_main.py
@@ -2,21 +2,10 @@ import sys
 sys.path.append('../..')
 
 import numpy as np
-import astropy.units as u
-from astropy.coordinates import SkyCoord
 from main.main_program_cluster_psp import main
 from sample_near_gaia import load_mock_data
 
-data = load_mock_data(0, 0, 0, 0.5, use_psp=True)
+data = load_mock_data(0, 0, 0, 3, parallax_cut=False)
 data = data[~np.any(np.isnan(data), axis=1)]
-'''
-galcen = SkyCoord(frame='galactocentric', representation_type='cartesian',
-                  x=data.T[0], y=data.T[1], z=data.T[2], 
-                  unit=[u.kpc, u.kpc, u.kpc])
-gal = galcen.transform_to('galactic')
-gal.representation_type = 'cartesian'
-mask = (gal.u.value**2 + gal.v.value**2 + gal.w.value**2 < 0.5**2)
-data = data[mask]
-'''
 
 main(None, 'local', data)
