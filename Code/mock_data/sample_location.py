aeff5492563055895f4e2dc22269181a6b874831
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-08 18:39:02 -0400

    added a module that sample location. Defined the function input

---
+++ Code/mock_data/sample_location.py
@@ -0,0 +1,12 @@
+"""
+NAME:
+    sample location
+
+PURPOSE:
+    A function that samples location under a given density function
+    
+HISTORY:
+    2018-07-05 - Written - Samuel Wong
+"""
+def sample_location(df, R_max, R_min, z_max, z_min, phi_max, phi_min):
+    
