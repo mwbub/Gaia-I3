e240e93bbd0a3a9a1cfeb9a016fd3accd94130fd
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-08 18:50:27 -0400

    created docstring of function

--- Code/mock_data/sample_location.py
+++ Code/mock_data/sample_location.py
@@ -6,7 +6,35 @@ PURPOSE:
     A function that samples location under a given density function
     
 HISTORY:
-    2018-07-05 - Written - Samuel Wong
+    2018-07-08 - Written - Samuel Wong
 """
-def sample_location(df, R_max, R_min, z_max, z_min, phi_max, phi_min):
-    
+
+def sample_location(df, n, R_max, R_min, z_max, z_min, phi_max, phi_min):
+    """
+    NAME:
+        sample_location
+
+    PURPOSE:
+        Given a density function and the maximum and minimum in cylindrical 
+        coordinate, as well as number of samples desired, sample location.
+        
+    INPUT:
+        df = a density function that takes input in galactocentric Cartesian
+             coordinate and return the normalized density at that point
+             
+        n = number of samples desired
+        
+        R_max, R_min = maximum and minimum radius (in natural units)
+        
+        z_max, z_min = maximum and minimum height (in natural units)
+        
+        phi_max, phi_min = maximum and minimum angle (in radian)
+
+    OUTPUT:
+        A numpy array in the form [(R, z, phi), (R, z, phi), ...] with n
+        components.
+
+
+    HISTORY:
+        2018-07-08 - Written - Samuel Wong
+    """
