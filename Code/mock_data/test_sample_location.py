7b5e2141260c2057df6c6297546abc937884b8b4
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-09 11:54:31 -0400

    imported axes in test

--- Code/mock_data/test_sample_location.py
+++ Code/mock_data/test_sample_location.py
@@ -6,10 +6,11 @@ from mpl_toolkits.mplot3d import Axes3D
 def df(R,z):
     return R**3
 
-location = sample_location(df,5000, 0, 1, -1, 1, 0, 2*np.pi)
+location = sample_location(df,1000, 0, 1, -1, 1, 0, 2*np.pi)
 R = location[:, 0]
 z = location[:, 1]
 phi = location[:, 2]
+print(np.shape(location))
 
 fig = plt.figure(figsize=(8, 8), facecolor='black')
 plt.style.use("dark_background")
