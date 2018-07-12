3bd3b3701911ef3a2de1572be0060f436edfbfa4
Mathew Bub <mathew.bub@gmail.com>
2018-07-12 00:55:52 -0400

    Finish toomredf class

--- Code/mock_data/toomredf/toomredf.py
+++ Code/mock_data/toomredf/toomredf.py
@@ -1,25 +1,63 @@
 import numpy as np
+from galpy.util.bovy_conversion import dens_in_msolpc3
 
 class toomredf:
-    def __init__(self, n=1, amp=1., r1=1., ro=8., vo=220.):
+    def __init__(self, n=1., ro=None, vo=None):
         self.n = n
-        self.amp = amp
-        self.r1 = r1
-        self.ro = ro
-        self.vo = vo
+        self.use_physical = False
         
+        if ro is None:
+            self.ro = 8.
+        else:
+            self.ro = ro
+            self.use_physical = True
+            
+        if vo is None:
+            self.vo = 220.
+        else:
+            self.vo = vo
+            self.use_physical = True
+    
+    def turn_physical_on(self):
+        self.use_physical = True
+    
+    def turn_physical_off(self):
+        self.use_physical = False
+        
+    def density(self, r, theta, use_physical=None):
+        if use_physical is None:
+            use_physical = self.use_physical
+            
+        result = self._S(theta)/(4*np.pi*r**2)
+        if use_physical:
+            result *= self.ro**2 * dens_in_msolpc3(self.vo, self.ro)
+            
+        return result
+    
+    def p_vphi(self, vphi, use_physical=None):
+        if use_physical is None:
+            use_physical = self.use_physical
+        
+        if use_physical:
+            vphi = vphi/self.vo
+            
+        return vphi**(2*self.n)*np.exp(-(self.n+1)*vphi**2)
+    
+    def p_vr(self, vr, use_physical=None):
+        if use_physical is None:
+            use_physical = self.use_physical
+            
+        if use_physical:
+            vr = vr/self.vo
+            
+        return np.exp(-(self.n+1)*vr**2)
+    
+    def p_vtheta(self, vtheta, use_physical=None):
+        return self.p_vr(vtheta, use_physical=use_physical)
+    
     def _p(self, theta):
         return (1+np.cos(theta))**(self.n+1) + (1-np.cos(theta))**(self.n+1)
     
     def _S(self, theta):
-            return 4*(self.n+1)*np.sin(theta)**(2*self.n)/self._p(theta)**2
-        
-    def pRtheta(self, r, theta, physical=False):
-        if physical:
-            r /= self.ro
-        return self.amp*(self.r1/r)**2*self._S(theta)
+        return 4*(self.n+1)*np.sin(theta)**(2*self.n)/self._p(theta)**2
     
-    def density(self, R, z, physical=False):
-        r = np.sqrt(R**2 + z**2)
-        theta = np.arcsin(R/r)
-        return self.pRtheta(r, theta, physical=physical)
