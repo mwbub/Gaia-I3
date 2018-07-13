f3cdd5cdbab104778b6419bca2e696cc02edf13f
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-13 16:55:21 -0400

    added sample velocity

--- Code/mock_data/sampling/sample_location.py
+++ Code/mock_data/sampling/sample_location.py
@@ -74,6 +74,50 @@ def sample_location(df, n, R_min, R_max, z_min, z_max, phi_min, phi_max):
     Rzphi_set = np.hstack((Rz_set, phi_set))
     return Rzphi_set
     
-    
+def sample_velocity(df, v_max, n):
+    """
+    NAME:
+        sample_location
+
+    PURPOSE:
+        Given a density function and the maximum and minimum in cylindrical 
+        coordinate, as well as number of samples desired, sample location.
+        
+    INPUT:
+        df = a distribution function that takes an array of velocity and
+            output their probability density
+             
+        v_max = maximum velocity allowed
+        
+        n = number of samples desired
+
+    OUTPUT:
+        a numpy array containing a list of n velocities 
+
+    HISTORY:
+        2018-07-13 - Written - Samuel Wong
+    """
+    # initialize list storing velocity result
+    v_set = []
+    # generate an array of random point in the cube: [0, v_max]x[0,1]
+    low =  (0, 0)
+    high = (v_max, 1)
+    # repeat while not enough points are generated yet
+    while len(v_set) < n:
+        # number of points to generate is the number of points missing
+        nmore = n - len(v_set)
+        # generate randome points in cube
+        v_ptrial = np.random.uniform(low, high, size=(nmore, 2))
+        v, p_trial = [v_ptrial[:, i] for i in range(2)]
+        # calculate the actual probability at these points
+        p = df(v)
+        # accept point if trial is less than real probability; in other words,
+        # accept if the point is below the curve
+        mask = p_trial < p
+        v_accept = v[mask]
+        v_set += v_accept.tolist()
+    # convert v set to array
+    v_set = np.array(v_set)
+    return v_set
     
     
