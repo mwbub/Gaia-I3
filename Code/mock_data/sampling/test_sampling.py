47c5e4e15ee41774d921ab2379226a141a57f605
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-14 23:10:46 -0400

    added statistical test of velocity sampling

--- Code/mock_data/sampling/test_sampling.py
+++ Code/mock_data/sampling/test_sampling.py
@@ -9,8 +9,8 @@ def df1(R,z):
 def df2(R,z):
     return 3*R*z**2
 
-def vdf(v):
-    return np.exp(-(v-1)**2/0.2)
+def vdf1(v):
+    return (1/np.sqrt(2*np.pi*(0.2**2)))*np.exp(-((v-40)**2)/(2*(0.2**2)))
 
 def test_sample_location(df, n, R_min, R_max, z_min, z_max, phi_min, phi_max,
                          df_max, std_R, std_z):
@@ -19,25 +19,29 @@ def test_sample_location(df, n, R_min, R_max, z_min, z_max, phi_min, phi_max,
     R = location[:, 0]
     z = location[:, 1]
     phi = location[:, 2]
-    """
+    
     fig = plt.figure(figsize=(8, 8), facecolor='black')
     plt.style.use("dark_background")
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(R*np.cos(phi), R*np.sin(phi), z, s = 10)
     plt.show()
-    """
+
     print('theoretical std of R =', std_R)
     print('std of R = ', np.std(R))
     print('theoretical std of z =', std_z)
     print('std of z = ', np.std(z))
 
-def test_sample_velocity():
-    v = sample_velocity(vdf, 3, 1000)
+def test_sample_velocity(vdf, v_max, n, vdf_max, std):
+    v = sample_velocity(vdf, v_max, n, vdf_max)
+    
+    print('theoretical std of v =', std)
+    print('experimental std of v=', np.std(v))
+    
     plt.figure()
     plt.style.use("dark_background")
     plt.hist(v)
     plt.show()
     
 test_sample_location(df1,1000000, 0, 1, -1, 1, 0, 2*np.pi, 2, 0.163, 0.577)
-#test_sample_location(df2, 1000000, 0, 1, -1, 1, 0, 2*np.pi, 3, 0.236, 0.775)
-#test_sample_velocity()
+test_sample_location(df2, 1000000, 0, 1, -1, 1, 0, 2*np.pi, 3, 0.236, 0.775)
+test_sample_velocity(vdf = vdf1, v_max = 80, n= 100000, vdf_max= 1.99, std =0.2)
