fixed the mistake of F_R with a missing factor of 1/R

Code/check_uniformity_of_density/Integral_of_Motion.py
@@ -186,7 +186,7 @@ def del_E(coord):
     R, phi, z, vR, vT, vz = cartesian_to_cylindrical(x, y, z, vx, vy, vz)
     # get the force of the potential in cylindrical form
     F_phi = evaluatephiforces(MWPotential2014, R, z, phi)
-    F_R = evaluateRforces(MWPotential2014, R, z, phi)
+    F_R = evaluateRforces(MWPotential2014, R, z, phi)/R
     F_z= evaluatezforces(MWPotential2014, R, z, phi)
     # return the gradient in Cartesian coordinate
     gradient = [F_phi*np.sin(phi) - F_R*np.cos(phi),