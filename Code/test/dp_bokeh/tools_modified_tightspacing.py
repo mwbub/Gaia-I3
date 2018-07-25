630193c768bd687338081f4473d2ac20a2ee2428
Michael Poon <michaelkm.poon@mail.utoronto.ca>
2018-07-25 10:53:59 -0400

    Updating dp_bokeh with functioning html files

--- Code/test/dp_bokeh/tools_modified_tightspacing.py
+++ Code/test/dp_bokeh/tools_modified_tightspacing.py
@@ -13,8 +13,9 @@ from galpy.util import bovy_coords
 from bokeh.io import output_file, show
 from bokeh.layouts import gridplot
 from bokeh.models import ColumnDataSource
-from bokeh.plotting import figure
 from bokeh.models.annotations import Title
+from bokeh.plotting import figure
+from bokeh.models import Label
 import pylab as plt
 
 
@@ -46,7 +47,6 @@ def color_plot_ij(source, TOOLS, i, j, all_proj, counter):
     x_axis, x_divisor = get_axis_from_index(i)
     y_axis, y_divisor = get_axis_from_index(j)
     
-    
     # create plot    
     proj = figure(tools=TOOLS, plot_width=300, plot_height=300)
     proj.scatter('{}'.format(x_axis), '{}'.format(y_axis), color='colours', size=0.5, source=source)
@@ -54,11 +54,10 @@ def color_plot_ij(source, TOOLS, i, j, all_proj, counter):
     proj.border_fill_color = None
     proj.xgrid.grid_line_alpha = 0.2
     proj.ygrid.grid_line_alpha = 0.2
-    proj.xaxis.axis_label = '{}/{}'.format(x_axis, x_divisor)
-    proj.yaxis.axis_label = '{}/{}'.format(y_axis, y_divisor)
     proj.background_fill_color = "black"
     
     all_proj[counter] = proj
+    
 
 def get_axis_from_index(i):
     """
@@ -86,7 +85,7 @@ def get_axis_from_index(i):
             ['vy', 'v_0'], ['vz', 'v_0']]
     return axis[i]
 
-def color_plot(source, TOOLS, TITLE):
+def color_plot(source, TOOLS):
     """
     NAME:
         color_plot
@@ -115,7 +114,7 @@ def color_plot(source, TOOLS, TITLE):
         for j in range(i + 1, 6):
             color_plot_ij(source, TOOLS, i, j, all_proj, counter)
             counter+=1
-    
+            
     # Adding titles and adjusting graph spacing
     t1 = Title()
     t1.text = 'Position Space Projections'
@@ -127,17 +126,93 @@ def color_plot(source, TOOLS, TITLE):
     t3.text = 'Position-Velocity Space Projections'
     all_proj[6].title = t3
     t4 = Title()
-    t4.text = TITLE
+    t4.text = 'Gaia Data e = 1, v_scale = 0'
     all_proj[2].title = t4
     all_proj[2].title.text_font_size = "12pt"
     
-    show(gridplot([[all_proj[0], None,        all_proj[2], all_proj[6], all_proj[9]], 
+    
+    #Row1
+    all_proj[0].yaxis.axis_label =  'y / R0'
+    all_proj[0].yaxis.axis_label_text_font_size = "12pt"
+    all_proj[0].xaxis.visible = False
+    all_proj[0].min_border_right = 0
+    all_proj[0].min_border_bottom = 0
+    #all_proj[0].plot_width=400
+    #all_proj[0].plot_height=400
+    all_proj[2].yaxis.axis_label =  'vx / v0'
+    all_proj[2].yaxis.axis_label_text_font_size = "12pt"
+    all_proj[2].min_border_right = 0
+    all_proj[2].min_border_bottom = 0
+    all_proj[2].xaxis.visible = False
+    all_proj[6].min_border_left = 0
+    all_proj[6].min_border_right = 0
+    all_proj[6].min_border_bottom = 0
+    all_proj[6].xaxis.visible = False
+    all_proj[6].yaxis.visible = False
+    all_proj[9].min_border_left = 0
+    all_proj[9].min_border_bottom = 0
+    all_proj[9].xaxis.visible = False
+    all_proj[9].yaxis.visible = False
+    
+    #Row2
+    all_proj[1].min_border_top = 0
+    all_proj[1].min_border_right = 0
+    all_proj[1].xaxis.axis_label =  'x / R0'
+    all_proj[1].xaxis.axis_label_text_font_size = "12pt"
+    all_proj[1].yaxis.axis_label =  'z / R0'
+    all_proj[1].yaxis.axis_label_text_font_size = "12pt"
+    all_proj[3].min_border_right = 0
+    all_proj[3].min_border_top = 0
+    all_proj[3].min_border_bottom = 0
+    all_proj[3].xaxis.visible = False
+    all_proj[3].yaxis.axis_label =  'vy / v0'
+    all_proj[3].yaxis.axis_label_text_font_size = "12pt"
+    all_proj[5].min_border_top = 0
+    all_proj[5].min_border_left = 0
+    all_proj[5].xaxis.axis_label =  'y / R0'
+    all_proj[5].xaxis.axis_label_text_font_size = "12pt"
+    all_proj[5].yaxis.visible = False
+    all_proj[7].min_border_left = 0
+    all_proj[7].min_border_right = 0
+    all_proj[7].min_border_top = 0
+    all_proj[7].min_border_bottom = 0
+    all_proj[7].xaxis.visible = False
+    all_proj[7].yaxis.visible = False
+    all_proj[10].min_border_left = 0
+    all_proj[10].min_border_top = 0
+    all_proj[10].min_border_bottom = 0
+    all_proj[10].yaxis.visible = False 
+    all_proj[10].xaxis.visible = False
+    
+    #Row3
+    all_proj[4].min_border_right = 0
+    all_proj[4].min_border_top = 0
+    all_proj[4].min_border_bottom = 0
+    all_proj[4].yaxis.axis_label =  'vz / v0'
+    all_proj[4].yaxis.axis_label_text_font_size = "12pt"
+    all_proj[8].min_border_left = 0
+    all_proj[8].min_border_right = 0
+    all_proj[8].min_border_top = 0
+    all_proj[8].min_border_bottom = 0
+    all_proj[8].yaxis.visible = False
+    all_proj[11].min_border_left = 0
+    all_proj[11].min_border_top = 0
+    all_proj[11].min_border_bottom = 0
+    all_proj[11].yaxis.visible = False
+    
+    
+    
+    #all_proj[6].yaxis.major_label_text_font_size = '0pt'
+    #proj.xaxis.axis_label = '{}/{}'.format(x_axis, x_divisor)
+    #proj.yaxis.axis_label = '{}/{}'.format(y_axis, y_divisor)
+    
+
+    show(gridplot([[all_proj[0], None,        all_proj[2], all_proj[6], all_proj[9]],
                    [all_proj[1], all_proj[5], all_proj[3], all_proj[7], all_proj[10]],
-                   [all_proj[12], None,       all_proj[4], all_proj[8], all_proj[11]],
-                   [all_proj[13], all_proj[14], None,     None,        None]]))
+                   [None,        None,        all_proj[4], all_proj[8], all_proj[11]]]))
         
-    # Show twice to compare when selecting regions    
     show(gridplot([[all_proj[0], None,        all_proj[2], all_proj[6], all_proj[9]], 
                    [all_proj[1], all_proj[5], all_proj[3], all_proj[7], all_proj[10]],
                    [all_proj[12], None,       all_proj[4], all_proj[8], all_proj[11]],
                    [all_proj[13], all_proj[14], None,     None,        None]]))
+
