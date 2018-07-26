fc4c57a3116e88a3d59b40d65bd968e410d897d2
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-26 11:49:03 -0400

    added docstring to evaluate dot, added a printline when anomaly happens

--- Code/check_uniformity_of_density/Uniformity_Evaluation.py
+++ Code/check_uniformity_of_density/Uniformity_Evaluation.py
@@ -192,38 +192,56 @@ def evaluate_uniformity_dot(f, points, v1, v2):
         evaluate_uniformity_dot
 
     PURPOSE:
-
+        Given a function <f> that takes (m,6) array of points, and (m,6) array
+        of points, <points>, as well as two list of (m,6) vectorrs, <v1> and
+        <v2>, return the dot product of gradient f at points witht the 
+        orthogonal complement of each pairs of rows in <v1> and <v2>. The four
+        dot products being zero implies that f is locally constant along
+        corresponding row of <v1> and <v2>.
 
     INPUT:
-
+        f = a differentiable function that takes an array of points, each with 
+            6 dimensions
+        points = (m,6) array, representing m points, each with n dimensions
+        v1, v2 = an array (m,6) vectors, where each row of v1 and v2 correspond
+                to a pair of vectors against which we are testing uniformity
 
     OUTPUT:
-
+        dot = an (m,4) array, where each row contains 4 dot product
+            corresponding to the result of the same row in points, v1, and v2
 
     HISTORY:
-        
+        2018-07-26 - Written - Samuel Wong
     """
-    m = np.shape(v1)[0]
+    m = np.shape(v1)[0] # get the number of rows
     W = []
+    # get the orthogonal complement in each pair of v1 and v2.
+    # print if anomaly in number of dimensions
     for i in range(m):
         comp = orthogonal_complement(np.array([v1[i], v2[i]]))
         if np.shape(comp) == (4,6):
             W.append(comp)
+        else:
+            print("Anomaly at row {}, v1 = {}, v2 = {} , complement = {}"\
+                  .format(i, v1[i], v2[i], comp))
     W = np.array(W)
-    W = np.reshape(W, (4*m, 6))
-    
+    # reshape so that all 4 orthgoonal vectors for each row are listed together
+    # with every 4 adjacent rows being complement of the same gradient
+    W = np.reshape(W, (4*m, 6)) 
+    # get the normalize gradient and duplicate four times
+    # reshape so that every 4 adjacent rows are identical, so that it matches
+    # with W to do dot product
     del_f_points = normalize(grad_multi(f, points))
     del_f_points2 = np.copy(del_f_points)
     del_f_points3 = np.copy(del_f_points)
     del_f_points4 = np.copy(del_f_points)
-    
     four_f = np.stack((del_f_points, del_f_points2, del_f_points3,
                        del_f_points4), axis =1)
     four_f = np.reshape(four_f, np.shape(W))
-    
+    # get the dot product and reshape such that each row contains 4 dot product
     dot = dot_product(four_f, W)
     dot = np.reshape(dot, (m, 4))
-    dot = dot.astype('float64')
+    dot = dot.astype('float64') # change data type from sympy back to float
     return dot
 
 def evaluate_uniformity_projection(points, f, v1, v2):
