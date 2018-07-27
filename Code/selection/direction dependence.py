41950a40db772a21a48d08559372facec6b7d552
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-27 13:09:56 -0400

    fixed a bug in number density so that it takes both 1d and 2d array, for meshgrid purpose

--- Code/selection/direction dependence.py
+++ Code/selection/direction dependence.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 ra_g, dec_g = np.linspace(1.,100.,100), np.linspace(1.,100.,100)
 ra_rv, dec_rv = np.linspace(1.,100.,100), np.linspace(1.,100.,100)
 #set variables for histogra
-ra_pixel = 0.1
-dec_pixel = 0.1
+ra_pixel = 10
+dec_pixel = 10
 ra_min = min(np.min(ra_g), np.min(ra_rv))
 dec_min = min(np.min(dec_g), np.min(dec_rv))
 ra_max = max(np.max(ra_g), np.max(ra_rv))
@@ -15,6 +15,7 @@ bin_ra = (ra_max - ra_min)/ra_pixel
 bin_dec = (dec_max - dec_min)/dec_pixel
 
 # get 2d histogram for gaia
+plt.figure(1)
 histogram_g, xedges, yedges, graph = plt.hist2d(ra_g, dec_g, 
                                                 bins = (bin_ra, bin_dec),
                                                 range = [[ra_min, ra_max],
@@ -26,6 +27,7 @@ plt.title("Density of Gaia in ra and dec")
 plt.savefig("Density of Gaia in ra and dec.png")
 
 # get 2d histogram for rv
+plt.figure(2)
 histogram_rv, xedges, yedges, graph = plt.hist2d(ra_rv, dec_rv, 
                                                 bins = (bin_ra, bin_dec),
                                                 range = [[ra_min, ra_max],
@@ -40,10 +42,19 @@ plt.savefig("Density of Gaia in ra and dec.png")
 def number_density(ra, dec, histogram):
     ra_index = ((ra-ra_min)/ra_pixel).astype(int)-1
     dec_index = ((dec-dec_min)/dec_pixel).astype(int)-1
-    result = []
-    for i in range(np.size(ra_index)):
-        result.append(histogram[ra_index[i]][dec_index[i]])
-    return np.array(result)
+    if np.ndim(ra) == 1:
+        result = []
+        for i in range(np.size(ra_index)):
+            result.append(histogram[ra_index[i]][dec_index[i]])
+        return np.array(result)
+    elif np.ndim(ra) == 2:
+        result = []
+        for i in range(np.shape(ra_index)[0]):
+            row = []
+            for j in range(np.shape(ra_index)[1]):
+                row.append(histogram[ra_index[i][j]][dec_index[i][j]])
+            result.append(row)
+        return np.array(result)
 
 # define the ratio of number density as a function of ra and dec
 def ratio(ra, dec):
@@ -54,4 +65,6 @@ ra_linspace = np.linspace(ra_min, ra_max, 100)
 dec_linspace = np.linspace(dec_min, dec_max, 100)
 ra_v, dec_v = np.meshgrid(ra_linspace, dec_linspace)
 z = ratio(ra_v, dec_v)
+plt.figure(3)
 plt.pcolor(ra_v, dec_v, z)
+plt.colorbar()
