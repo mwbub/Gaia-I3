6da20be918e8cec386a9a4edd754a0c9b9fcaaf2
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-07-27 12:13:28 -0400

    added the historram for rv

--- Code/selection/direction dependence.py
+++ Code/selection/direction dependence.py
@@ -14,7 +14,7 @@ dec_max = np.max(np.max(dec_g), np.max(dec_rv))
 bin_ra = (ra_max - ra_min)/ra_pixel
 bin_dec = (dec_max - dec_min)/dec_pixel
 
-# get 2d histogram
+# get 2d histogram for gaia
 histogram_g, xedges, yedges, graph = plt.hist2d(ra_g, dec_g, 
                                                 bins = (bin_ra, bin_dec),
                                                 range = [[ra_min, ra_max],
@@ -22,8 +22,19 @@ histogram_g, xedges, yedges, graph = plt.hist2d(ra_g, dec_g,
 plt.colorbar()
 plt.xlabel('ra(mas)')
 plt.ylabel('dec(mas)')
-plt.title("Density of Gaia in Ra and Dec")
-plt.savefig("Density of Gaia in Ra and Dec.png")
+plt.title("Density of Gaia in ra and dec")
+plt.savefig("Density of Gaia in ra and dec.png")
+
+# get 2d histogram for rv
+histogram_rv, xedges, yedges, graph = plt.hist2d(ra_rv, dec_rv, 
+                                                bins = (bin_ra, bin_dec),
+                                                range = [[ra_min, ra_max],
+                                                         [dec_min, dec_max]])
+plt.colorbar()
+plt.xlabel('ra(mas)')
+plt.ylabel('dec(mas)')
+plt.title("Density of RV in ra and dec")
+plt.savefig("Density of Gaia in ra and dec.png")
 
 
 def number_density(ra, dec, histogram):
