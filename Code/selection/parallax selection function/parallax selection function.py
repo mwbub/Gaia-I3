88e95e9a1414c42926e0f24d1dde67af5c73eb9f
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-08-08 01:46:27 -0400

    fixed the issue of bins size being one point more than the histogram by plotting the centralized bin point

--- Code/selection/parallax selection function/parallax selection function.py
+++ Code/selection/parallax selection function/parallax selection function.py
@@ -6,10 +6,10 @@ from astropy.table import Table
 data_g = Table.read("gaia_data_with_straight_cutoff.fits")
 data_rv = Table.read("gaia_rv_with_straight_cutoff.fits")
 
-# get rid of stars with negative parallax
-mask_g = np.all(np.array([data_g["parallax"]>0, 1/data_g["parallax"]<30]),axis = 0)
+# get rid of stars with negative parallax and stars further than 1 kpc
+mask_g = np.all(np.array([data_g["parallax"]>0, 1/data_g["parallax"]<1]),axis = 0)
 data_g = data_g[mask_g]
-mask_rv = np.all(np.array([data_rv["parallax"]>0, 1/data_rv["parallax"]<30]),axis = 0)
+mask_rv = np.all(np.array([data_rv["parallax"]>0, 1/data_rv["parallax"]<1]),axis = 0)
 data_rv = data_rv[mask_rv]
 
 # get parallax
@@ -17,11 +17,11 @@ parallax_g = data_g["parallax"]
 parallax_rv = data_rv["parallax"]
 
 #set up min and max
-parallax_min = min(np.min(parallax_g), np.min(parallax_rv))
-parallax_max = min(max(np.max(parallax_g), np.max(parallax_rv)),10)
-#set up a list of even bins of 0.01 kpc in distance space, convert to uneven
+parallax_min = 1
+parallax_max = 10
+#set up a list of even bins of 0.001 kpc in distance space, convert to uneven
 #parralax bins
-distance_pixel = 0.01
+distance_pixel = 0.001
 bins = np.flip(1/np.arange(1/parallax_max,1/parallax_min + distance_pixel, distance_pixel), axis =0)
 
 #plot rv paralax distribution
@@ -44,8 +44,15 @@ plt.savefig("Parallax Distribution in Gaia.png")
 
 #plot ratio
 plt.figure()
-bins_adjusted = np.flip(1/np.arange(1/parallax_max,1/parallax_min, distance_pixel), axis =0)
-plt.plot(bins_adjusted, hist_parallax_rv/hist_parallax_g)
+# since bins store the edge of each bin, it has one more point than the
+# histogram. We want the x-array to be the center of each bin.
+# Take every element except the first, subtract every element except the last.
+# This gives an array of one less element representing the difference between
+# each entry. Divide by 2 to find the distance between every two neighboring
+# points. Add this back to every points except the last to get centralized
+# points
+bins_centralized = bins[:-1] + (bins[1:] - bins[:-1])/2
+plt.plot(bins_centralized, hist_parallax_rv/hist_parallax_g)
 plt.xlabel('Parallax')
 plt.ylabel('Ratio (RV/G)')
 plt.title("Ratio of Freqeuncy of Parallax")
