74b5b48370b2e27edb0ffa97b9268b98c3784137
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-08-08 20:03:22 -0400

    restored original sample location and make a new sample location selection function

--- Code/mock_data/sampling/sampling.py
+++ Code/mock_data/sampling/sampling.py
@@ -15,11 +15,78 @@ HISTORY:
 import numpy as np
 from scipy import interpolate
 
-def sample_location(df, n, R_min, R_max, z_min, z_max, phi_min, phi_max,
-                    df_max, selection = None, R_0 = 8.3, z_0 = 0.):
+def sample_location(df, n, R_min, R_max, z_min, z_max, phi_min, phi_max, df_max):
     """
     NAME:
         sample_location
+
+    PURPOSE:
+        Given a density function and the maximum and minimum in cylindrical 
+        coordinate, as well as number of samples desired, sample location.
+        
+    INPUT:
+        df = a density function that takes input in galactocentric Cylindrical
+             coordinate and return the normalized density at that point;
+             Assume it takes array in the form 
+             df(array([R,R,..]),array([z,z,...]))
+             
+        n = number of samples desired
+        
+        R_max, R_min = maximum and minimum radius (in natural units)
+        
+        z_max, z_min = maximum and minimum height (in natural units)
+        
+        phi_max, phi_min = maximum and minimum angle (in radian)
+        
+        df_max = maximum value of the dsitribution function
+
+    OUTPUT:
+        A numpy array in the form [(R, z, phi), (R, z, phi), ...] with n
+        components.
+
+
+    HISTORY:
+        2018-07-08 - Written - Samuel Wong
+        2018-07-13 - Modified so that df assumed to take arrays - Samuel Wong
+    """
+    # initialize list storing [[R,z], [R,z],..] result
+    Rz_set = []
+    # generate an array of random point in the cube:
+    #[R_min, R_max]x[z_min, z_max]x[0,df_max]
+    # this is effectively random points in Rz space and a random trial height
+    low =  (R_min, z_min, 0)
+    high = (R_max, z_max, df_max)
+    # repeat while not enough points are generated yet
+    while len(Rz_set) < n:
+        # number of points to generate is the number of points missing
+        nmore = n - len(Rz_set)
+        # generate randome points in cube
+        R_z_ptrial = np.random.uniform(low, high, size=(nmore, 3))
+        R, z, p_trial = [R_z_ptrial[:, i] for i in range(3)]
+        # calculate the actual probability at these points
+        p = df(R,z)
+        # accept point if trial is less than real probability; in other words,
+        # accept if the point is below the curve
+        mask = p_trial < p
+        R_accept = R[mask]
+        z_accept = z[mask]
+        Rz_accept = np.stack((R_accept, z_accept), axis = 1)
+        # add accepted points into stored list
+        Rz_set += Rz_accept.tolist()
+    # convert Rz set to array
+    Rz_set = np.array(Rz_set)
+    # get a unifrom distribution in phi
+    phi_set = np.reshape(np.random.uniform(phi_min, phi_max, n), (n, 1))
+    Rzphi_set = np.hstack((Rz_set, phi_set))
+    return Rzphi_set
+
+
+def sample_location_selection(df, n, R_min, R_max, z_min, z_max, phi_min,
+                              phi_max, df_max, selection, R_0 = 8.3, z_0 = 0.,
+                              phi_0 = 0.):
+    """
+    NAME:
+        sample_location_selection
 
     PURPOSE:
         Given a density function and the maximum and minimum in cylindrical 
@@ -51,9 +118,7 @@ def sample_location(df, n, R_min, R_max, z_min, z_max, phi_min, phi_max,
 
 
     HISTORY:
-        2018-07-08 - Written - Samuel Wong
-        2018-07-13 - Modified so that df assumed to take arrays - Samuel Wong
-        2018-08-08 - Added optional selection function - Samuel Wong
+        2018-08-08 - Written - Samuel Wong
     """
     # initialize list storing [[R,z], [R,z],..] result
     Rz_set = []
@@ -76,6 +141,10 @@ def sample_location(df, n, R_min, R_max, z_min, z_max, phi_min, phi_max,
         mask = p_trial < p
         R_accept = R[mask]
         z_accept = z[mask]
+        # if selection function is given, add another cut and mask
+        if selection is not None:
+            # calculate distance to Sun
+            distance = R
         Rz_accept = np.stack((R_accept, z_accept), axis = 1)
         # add accepted points into stored list
         Rz_set += Rz_accept.tolist()
