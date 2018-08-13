eefc562972ed567252a013ea26ce8b4f14eb9872
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-08-13 16:44:23 -0400

    wrote module that runmain on qdf selection

--- Code/mock_data/qdf/run_main_qdf.py
+++ Code/mock_data/qdf/run_main_qdf.py
@@ -4,7 +4,7 @@ sys.path.append('../..')
 sys.path.append('../../check_uniformity_of_density')
 
 from main.main_program_cluster import main
-qdf_sample = np.load("data/qdf sample cartesian physical, date=(2018, 8, 10).npy")
+qdf_sample = np.load("data/qdf sample cartesian physical with selection, date=(2018, 8, 13).npy")
 
 main(uniformity_method = "projection", gradient_method = "analytic",
      search_method = "local", custom_density = None, custom_samples = qdf_sample,
