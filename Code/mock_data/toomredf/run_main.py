be2a7a6a158bc921f6733dc189c06af4ae6d5d23
Mathew Bub <mathew.bub@gmail.com>
2018-08-13 15:04:58 -0400

    Change settings on run_main.py

--- Code/mock_data/toomredf/run_main.py
+++ Code/mock_data/toomredf/run_main.py
@@ -8,10 +8,9 @@ from sample_toomredf import sample_like_gaia
 from toomredf import ToomrePotential
 
 n = 4
-epsilon = 4
+epsilon = 1
 
-centres = np.load('main_program_results/toomre_main/data.npz')['cluster']
 data = sample_like_gaia(n, epsilon)
 pot = ToomrePotential(n)
 main(uniformity_method='projection', gradient_method='numeric', 
-     custom_samples=data, custom_centres=centres, custom_potential=pot)
+     custom_samples=data, custom_potential=pot)
