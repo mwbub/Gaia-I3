3b721bc42bfc45f495964544d1a5eadaabf717e6
Michael Poon <michaelkm.poon@mail.utoronto.ca>
2018-08-16 21:59:44 -0400

    ran gaia e=0.5, e=1 with selection function

--- Code/main/main_program_cluster.py
+++ Code/main/main_program_cluster.py
@@ -290,7 +290,8 @@ def main(uniformity_method = "projection", gradient_method = "analytic",
                   custom_potential)
        
   
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    
     main(uniformity_method = "projection", gradient_method = "analytic",
          search_method = "local", custom_density = None, custom_samples = None,
          custom_centres = None, custom_potential = None,
