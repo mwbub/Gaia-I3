2689e676d06e89e9b284f980a219d48bd39f4b6b
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-08-23 12:04:53 -0400

    modify selection so that it uses the new selection function. And import qdf now that I start using developer.

--- Code/mock_data/qdf/qdf_sample_with_selection.py
+++ Code/mock_data/qdf/qdf_sample_with_selection.py
@@ -9,30 +9,32 @@ import time as time_class
 #import qdf related things
 from galpy.potential import MWPotential2014
 from galpy.actionAngle import actionAngleAdiabatic
+from galpy.df import quasiisothermaldf
 aA= actionAngleAdiabatic(pot=MWPotential2014,c=True)
 # set up qdf
 # qdf not imported from galpy since I am running a modified galpy in console
 qdf= quasiisothermaldf(1./3.,0.2,0.1,1.,1.,pot=MWPotential2014,aA=aA,cutcounter=True)
 #get selection function
-with open("../../selection/parallax selection function/selection_function", "rb") as dill_file:
+with open("../../selection/parallax selection with galactic plane/selection_function", "rb") as dill_file:
     selection_function = dill.load(dill_file)
 
 start = time_class.time()
 #sample location
 # get the maximum of qdf
-qdf_max = 1.2*qdf.density(0.9375, 0)
+qdf_max = 1.2*qdf.density(0.875, 0)
 # sample R from 7.5 kpc to 8.5 kpc; sample z from -0.5 kpc to 0.5 kpc
 # let phi range from -arctan(0.5/8) to arctan(0.5/8)
-location = sample_location_interpolate_selection(df=qdf.density,n=1700000,
-                                                 R_min=0.9375, R_max=1.0625,
-                                                 z_min=-0.0625, z_max=0.0625,
-                                                 phi_min=-0.0624,
-                                                 phi_max=0.0624,
+location = sample_location_interpolate_selection(df=qdf.density,n=3100000,
+                                                 R_min=0.875, R_max=1.125,
+                                                 z_min=-0.125, z_max=0.125,
+                                                 phi_min=-0.124,
+                                                 phi_max=0.124,
                                                  df_max = qdf_max,
                                                  pixel_R = 0.01,
                                                  pixel_z = 0.01,
                                                  selection=selection_function,
-                                                 R_0 = 1.0,z_0 = 0.,phi_0 = 0.)
+                                                 R_0 = 1.0,z_0 = 0.,phi_0 = 0.,
+                                                 directional_dependence = True)
 # sample velocity
 # get the R, z and phi colum
 R,z,phi = location.T
@@ -48,7 +50,7 @@ physical_rect = to_physical_units(coord_rect)
 #get current date
 date = time_class.localtime(time_class.time())[0:3]
 #save data
-np.save("qdf sample cartesian physical with selection, date={}.npy".format(str(date)), physical_rect)
+np.save("qdf sample cartesian physical with new selection, e=1, date={}.npy".format(str(date)), physical_rect)
 
 end = time_class.time()
 print('time =', end - start)
