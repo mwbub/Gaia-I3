dac340b3859c72e338db55d13249d910ce243fb3
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2018-08-23 12:06:54 -0400

    run main with new selection and fixed handedness problem

--- Code/mock_data/qdf/run_main_qdf.py
+++ Code/mock_data/qdf/run_main_qdf.py
@@ -1,11 +1,16 @@
 import numpy as np
 import sys
+import dill
 sys.path.append('../..')
 sys.path.append('../../check_uniformity_of_density')
 
 from main.main_program_cluster import main
-qdf_sample = np.load("data/qdf sample cartesian physical with selection, date=(2018, 8, 13).npy")
+qdf_sample = np.load("data/qdf sample cartesian physical with new selection, e=1, date=(2018, 8, 20).npy")
+qdf_sample[:,0] = -qdf_sample[:,0] # fix orientation by adding negative sign in front of x
+with open("../../selection/parallax selection with galactic plane/selection_function","rb") as dill_file:
+    selection = dill.load(dill_file)
 
 main(uniformity_method = "projection", gradient_method = "analytic",
-     search_method = "local", custom_density = None, custom_samples = qdf_sample,
-     custom_centres = None)
+         search_method = "local", custom_density = None, custom_samples = qdf_sample,
+         custom_centres = None, custom_potential = None,
+         selection = selection, band_width = 5)
